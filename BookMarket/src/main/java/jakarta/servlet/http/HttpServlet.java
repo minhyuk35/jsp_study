00642ac9e0b0704f3f89f63c6dc01ba0b7f51e3e
minhyuk35 <135953567+minhyuk35@users.noreply.github.com>
2026-03-20 11:17:11 +0900

    Delete BookMarket directory

--- BookMarket/src/main/java/jakarta/servlet/http/HttpServlet.java
+++
@@ -1,7 +0,0 @@
-package jakarta.servlet.http;
-
-// Minimal stub to satisfy Eclipse JSP validator which expects this class on the build path.
-// NOTE: This is a lightweight local stub for editor/validation only. Remove before packaging for a servlet container.
-public class HttpServlet {
-    // Intentionally empty
-}
